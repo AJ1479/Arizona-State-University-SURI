7f2549b68a299e261f0e79e7b5795bed25318de3
AJ1479 <arushijain1479@gmail.com>
2022-07-21 16:32:24 +0530

    feat: Added DB, routes for questions

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
-
+const mongoose = require('mongoose');
 const path = require('path')
 const express = require('express');
-// const ejs = require('ejs');
 const bodyParser = require('body-parser')
+
+require('./models/db');
+
+
+const Question = require('./models/questionSchema');
+const User = require('./models/userSchema');
+const Input = require('./models/responsesSchema');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -16,7 +22,24 @@ app.use('/public', express.static("public"));
 app.use('/react_deploy', express.static("react_deploy"));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'hello' });
+    res.sendFile(__dirname+'\\views\\login.html');
 })
+
+app.post('/login', async(req, res) => {
+    console.log(req.body);
+    const user = new User({
+        ID: req.body.userID,
+  });
+  await User.create(user);
+    res.sendFile(__dirname+'\\views\\question.html');
+})
+
+
+app.post('/:questionNumber', (req, res) => {
+    questionNumber = req.params.questionNumber;
+    console.log(req.body);
+    res.sendFile(__dirname+`\\views\\question${questionNumber}.html`);
+})
+
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
