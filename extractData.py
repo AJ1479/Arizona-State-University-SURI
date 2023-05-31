025a1b47a091cb68d764425386cc7d1ecd2aa10f
HarikaBhogaraju <o_are_lab@new-vm.us-central1-a.c.bubbly-journey-283623.internal>
2023-05-31 22:01:00 +0000

    adding demographic data

--- extractData.py
+++ extractData.py
@@ -16,52 +16,26 @@ usersCol = db['users']
 inputsCol = db['inputs']
 count = 0
 dataArray = []
-for u in inputsCol.find():
-    userResponse = {}
-    uid = u['UserID']
-    userResponse['userID'] = uid
 
-    if(len(u['Response']) == 10):
+for x in usersCol.find():
+    print(len(x))
+    if(len(x) == 14):
         count = count+1
-        print(uid)
-        for i in range(10):
-            inp1 = u['Response'][i]["input1"]
-            print(u['Response'][i]["input1"])
-            
-            inp2  = u['Response'][i]["input2"]
-            print(u['Response'][i]["input2"])
+        userResponse = []
+        id = x['ID']
+        ed = x['education']
+        ex = x['experience']
 
-            inp3  = u['Response'][i]["input3"]
-            print(u['Response'][i]["input3"])
-            
-            inp4 = u['Response'][i]["input4"]
-            print(u['Response'][i]["input4"])
-            
-            inp5 = u['Response'][i]["input5"]
-            print(u['Response'][i]["input5"])
-            
-            inp6 = u['Response'][i]["input6"]
-            print(u['Response'][i]["input6"])
-            
-            inp7 = u['Response'][i]["input7"]
-            print(u['Response'][i]["input7"])
-            
-            inp8 = u['Response'][i]["input8"]
-            print(u['Response'][i]["input8"])
-            
-            #print(u['Response'][i]["input9"])
-            #print(u['Response'][i]["input10"])
-            userResponse[i] = { "input1" : inp1, "input2": inp2, "input3": inp3,  "input4": inp4,  "input5": inp5, "input6": inp6,  "input7": inp7,  "input8": inp8}
-        print("")
-        dataArray.append(userResponse)
-    else:
-        print(uid)
-        print(len(u['Response']))
+        if(ed != "" and ex != "" and id != ""):
+            userResponse=[id,ed,ex]
 
-#print(count)
-#print(dataArray)
+    print(userResponse)
+    dataArray.append(userResponse)
+
+print(count)
+print(dataArray)
 rightNow = datetime.datetime.today().strftime('%m-%d-%Y')
-file_name = rightNow + dbase + ".json"
+file_name = rightNow + dbase + "-users.json"
 
 with open(str(file_name), 'w+') as outfile:
         json.dump(dataArray, outfile)
